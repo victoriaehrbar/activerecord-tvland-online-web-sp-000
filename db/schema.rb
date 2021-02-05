25eae62d7ee458c4bf88d3c93ecf4cbe7e594c0d
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-05 01:54:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 9) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -28,6 +33,8 @@ ActiveRecord::Schema.define(version: 7) do
     t.string "name"
     t.integer "network_id"
     t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
